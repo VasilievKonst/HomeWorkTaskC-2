ec8c358289f81a64bba348ab3f1f3697abd99ecc
KonstantinVasiliev <grafit21@mail.ru>
2022-10-22 10:27:45 +0500

    Save first metod Task10

---
+++ Task10/Program.cs
@@ -0,0 +1,9 @@
+﻿// Два вида решения в разных ветках.
+// 1 способ
+
+Console.Write("Введите трехзначное число: ");
+string str = Console.ReadLine();
+
+Char[] numb = str.ToCharArray();
+Console.Write("Второй символ трехзначного числа равен: ");
+Console.WriteLine(numb[1]);
