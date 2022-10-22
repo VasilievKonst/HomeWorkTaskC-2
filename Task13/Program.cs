1111cf9693818e97aaf77eecbb365cb75b922e0f
KonstantinVasiliev <grafit21@mail.ru>
2022-10-22 23:05:27 +0500

    save second variant on branch VariantS

---
+++ Task13/Program.cs
@@ -0,0 +1,18 @@
+﻿// Вариант №2  в котором число преобразовано в вид массива
+
+int[] array = {1, 23};
+int n = array.Length;
+Console.WriteLine(n);
+void ThirdEL()
+{
+    if (n <= 2)
+    {
+        Console.WriteLine("Третий элемент в числе отсутствует! Ошибка!");
+    }
+    else
+    {
+        Console.Write("Третьим элементом заданного числа является: ");
+        Console.WriteLine(array[2]);
+    }
+}
+ThirdEL();
