728fa4674faceb32e721bbe8e3a0797529c59443
KonstantinVasiliev <grafit21@mail.ru>
2022-10-22 17:20:56 +0500

    add method void task10 first variant

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -3,7 +3,10 @@
 
 Console.Write("Введите трехзначное число: ");
 string str = Console.ReadLine();
-
+void printres()
+{
 Char[] numb = str.ToCharArray();
 Console.Write("Второй символ трехзначного числа равен: ");
 Console.WriteLine(numb[1]);
+}
+printres();
