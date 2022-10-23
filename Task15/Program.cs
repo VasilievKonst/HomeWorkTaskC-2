e60d789d4a142ab51563335b2c59c4a1b05364a6
KonstantinVasiliev <grafit21@mail.ru>
2022-10-23 12:11:28 +0500

    Save task15

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.Write("Ведите число соответствующее дню недели: ");
+string n = Console.ReadLine();
+int Day = Convert.ToInt32(n);
+Console.Write("Является ли день недели выходным: ");
+void Whatday()
+{
+    if (Day < 6)
+    {
+        Console.WriteLine("Нет");
+    }
+    if (Day > 5)
+    {
+        Console.WriteLine("Да");
+    }
+}
+Whatday();
